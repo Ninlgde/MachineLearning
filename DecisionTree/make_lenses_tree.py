c6094db6f7fb3cc3d75b4f0ede66e2d606ba1eac
Ninlgde <ninlgde.ma@gmail.com>
2016-02-27 02:35:30 +0800

    decision tree algorithm

---
+++ DecisionTree/make_lenses_tree.py
@@ -0,0 +1,20 @@
+# -*- coding=utf-8 -*-
+'''
+make_lenses_tree
+Date: 16/2/27
+Company: Copyright (c) 2016 Ninlgde co.,Ltd. All right reserved.
+'''
+
+import trees
+import treePlotter as tp
+
+__author__ = 'Ninlgde'
+
+if __name__ == '__main__':
+    fr = open('lenses.txt')
+    lenses = [inst.strip().split('\t') for inst in fr.readlines()]
+    lensesLabels = ['age', 'prescript', 'astigmatic', 'tearRate']
+    lensesTree = trees.createTree(lenses, lensesLabels)
+    trees.storeTree(lensesTree, 'lenses_tree.bin')
+    tree = trees.grabTree('lenses_tree.bin')
+    tp.createPlot(tree)
